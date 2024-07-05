a6669f15d1ab43c3b02e13919d623be716d79bbb
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-07-05 20:49:55 +0530

    feat: added unimplemented error for problem api

--- src/controllers/problem.controller.ts
+++ src/controllers/problem.controller.ts
@@ -1,17 +1,37 @@
 import { Request, Response } from "express";
 
 export function pingProblemController(req: Request, res: Response) {
-  res.send({
+  return res.send({
     message: "Problem Controller is up and running",
   });
 }
 
-export function addProblem(req: Request, res: Response) {}
+export function addProblem(req: Request, res: Response) {
+  return res.status(501).json({
+    message: "Not Implemented",
+  });
+}
 
-export function getProblem(req: Request, res: Response) {}
+export function getProblem(req: Request, res: Response) {
+  return res.status(501).json({
+    message: "Not Implemented",
+  });
+}
 
-export function getProblems(req: Request, res: Response) {}
+export function getProblems(req: Request, res: Response) {
+  return res.status(501).json({
+    message: "Not Implemented",
+  });
+}
 
-export function deleteProblem(req: Request, res: Response) {}
+export function deleteProblem(req: Request, res: Response) {
+  return res.status(501).json({
+    message: "Not Implemented",
+  });
+}
 
-export function updateProblem(req: Request, res: Response) {}
+export function updateProblem(req: Request, res: Response) {
+  return res.status(501).json({
+    message: "Not Implemented",
+  });
+}
